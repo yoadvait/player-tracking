28baca8b0a89fad45b35f8139c1cad3e0f637287
Advait <aviadvait786@gmail.com>
2025-06-30 01:04:59 +0530

    first commit

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+Player Re-identification System
+Advanced tracking and re-identification for sports footage
+"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+
+from .detection import *
+from .tracking import *
+from .reidentification import *
+from .features import *
+from .evaluation import *
+from .visualization import *
